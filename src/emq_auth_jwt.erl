9a14ede4769091b8c104bb4fad0d4a147ebcb9dd
Duan <JDuan@mi.corp.rockfin.com>
2016-12-06 14:33:25 -0500

    Fix bugs

--- src/emq_auth_jwt.erl
+++ src/emq_auth_jwt.erl
@@ -6,12 +6,14 @@
 
 -export([init/1, check/3, description/0]).
 
+-define(EMPTY(Username), (Username =:= undefined orelse Username =:= <<>>)).
+
 init(Opts) -> {ok, Opts}.
 
 check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts) when ?EMPTY(Username); ?EMPTY(Password)  ->
     {error, username_or_token_undefined};
 
-check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts) when ?EMPTY(Username); ?EMPTY(Password)  ->
+check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts)  ->
     io:format("Auth jwt: clientId=~p, username=~p, password=~p~n",
               [ClientId, Username, Password]),
     ok.
