8e249d975470665392022c55b12af2ee17578a4e
Duan <JDuan@mi.corp.rockfin.com>
2016-12-06 15:53:44 -0500

    work around

---
+++ src/emq_auth_jwt_app.erl
@@ -0,0 +1,20 @@
+-module(emq_auth_jwt_app).
+
+-include("emq_auth_jwt.hrl").
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    {ok, Sup} = emq_auth_jwt_sup:start_link(),
+    ok = emqttd_access_control:register_mod(auth, emq_auth_jwt, []),
+    ok = emqttd_access_control:register_mod(acl, emq_acl_jwt, []),
+    emq_plugin_template:load(application:get_all_env()),
+    {ok, Sup}.
+
+stop(_State) ->
+    ok = emqttd_access_control:unregister_mod(auth, emq_auth_jwt),
+    ok = emqttd_access_control:unregister_mod(acl, emq_acl_jwt),
+    emq_plugin_template:unload().
