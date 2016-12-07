badb5d527ec0104b6bccc738afb7fa38dcc5ac32
Duan <JDuan@mi.corp.rockfin.com>
2016-12-07 17:27:58 -0500

    Add jwt

--- src/emq_auth_jwt.erl
+++ src/emq_auth_jwt.erl
@@ -8,6 +8,8 @@
 
 -define(UNDEFINED(S), (S =:= undefined orelse S =:= <<>>)).
 
+-define(SECRET, <<"password">>)
+
 init(Opts) -> {ok, Opts}.
 
 check(#mqtt_client{username = Username}, Password, _Opts) when ?UNDEFINED(Username); ?UNDEFINED(Password)  ->
@@ -16,6 +18,8 @@ check(#mqtt_client{username = Username}, Password, _Opts) when ?UNDEFINED(Userna
 check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts)  ->
     io:format("Auth jwt: clientId=~p, username=~p, password=~p~n",
               [ClientId, Username, Password]),
+    Claims = jwt:decode(password, ?SECRET),
+    io:format("Claims : ~p", Claims),
     ok.
 
 description() -> "Auth jwt Module".
