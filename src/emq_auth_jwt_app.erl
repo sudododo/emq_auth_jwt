9f2d063cdf7378711de9ca24ed4c9461d8128af2
Duan <JDuan@mi.corp.rockfin.com>
2016-12-07 14:49:44 -0500

    Work around

--- src/emq_auth_jwt_app.erl
+++ src/emq_auth_jwt_app.erl
@@ -7,8 +7,8 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emq_auth_jwt_sup:start_link(),
-    emqttd_access_control:register_mod(auth, emq_auth_jwt, Opts),
-    emqttd_access_control:register_mod(acl, emq_acl_jwt, Opts),
+    emqttd_access_control:register_mod(auth, emq_auth_jwt),
+    emqttd_access_control:register_mod(acl, emq_acl_jwt),
     {ok, Sup}.
 
 prep_stop(State) ->
