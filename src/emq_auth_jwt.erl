c289bb640e263592755fd273684960d4bb2d7bae
Duan <JDuan@mi.corp.rockfin.com>
2016-12-07 14:30:05 -0500

    Work around

--- src/emq_auth_jwt.erl
+++ src/emq_auth_jwt.erl
@@ -10,8 +10,8 @@
 
 init(Opts) -> {ok, Opts}.
 
-check(#mqtt_client{username = Username}, Password, _Opts) when ?EMPTY(Username); ?EMPTY(Password)  ->
-    {error, username_or_token_undefined};
+% check(#mqtt_client{username = Username}, Password, _Opts) when ?EMPTY(Username); ?EMPTY(Password)  ->
+%     {error, username_or_token_undefined};
 
 check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts)  ->
     io:format("Auth jwt: clientId=~p, username=~p, password=~p~n",
