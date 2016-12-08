227f27d339e48d7af7bce0cf8fe4c2866af5c03a
Duan <JDuan@mi.corp.rockfin.com>
2016-12-08 16:41:59 -0500

    Work around

--- src/emq_auth_jwt.erl
+++ src/emq_auth_jwt.erl
@@ -22,10 +22,10 @@ check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts) 
               [ClientId, Username, Password]),
     Token = <<"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIn0.Q6CM1qIz2WTgTlhMzpFL8jI8xbu9FFfj5DY_bGVY98Y">>,
     case jwt:decode(Token, ?SECRET) of
+        {ok, claimsJSON} -> io:format("claimsJSON"), {ok, "claimsJSON"};
         {error, invalid_token} -> io:format("invalid_token"), {error, "invalid_token"};
         {error, invalid_signature} -> io:format("invalid_signature"), {error, "invalid_signature"};
-        {error, expired} -> io:format("invalid_signature"), {error, "invalid_signature"};
-        {ok, claimsJSON} -> io:format("claimsJSON"), {ok, "claimsJSON"}
+        {error, expired} -> io:format("invalid_signature"), {error, "invalid_signature"}
     end.
 
 description() -> "Auth jwt Module".
