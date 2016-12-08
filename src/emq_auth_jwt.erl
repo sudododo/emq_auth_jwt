f25f2c4fa1fa28e1676ad19c5cec59f088466a46
Duan <JDuan@mi.corp.rockfin.com>
2016-12-08 14:16:00 -0500

    Work around

--- src/emq_auth_jwt.erl
+++ src/emq_auth_jwt.erl
@@ -18,11 +18,12 @@ check(#mqtt_client{username = Username}, Password, _Opts) when ?UNDEFINED(Userna
 check(#mqtt_client{client_id = ClientId, username = Username}, Password, _Opts)  ->
     io:format("Auth jwt: clientId=~p, username=~p, password=~p~n",
               [ClientId, Username, Password]),
-    case jwt:decode(Password, ?SECRET) of
-        {error, invalid_token} -> io:format("invalid_token"), {error, "invalid_token"};
-        {error, invalid_signature} -> io:format("invalid_signature"), {error, "invalid_signature"};
-        {error, expired} -> io:format("invalid_signature"), {error, "invalid_signature"};
-        {ok, claimsJSON} -> io:format("claimsJSON"), {ok, "claimsJSON"}
+    jwt:decode(Password, ?SECRET)
+    % case jwt:decode(Password, ?SECRET) of
+    %     {error, invalid_token} -> io:format("invalid_token"), {error, "invalid_token"};
+    %     {error, invalid_signature} -> io:format("invalid_signature"), {error, "invalid_signature"};
+    %     {error, expired} -> io:format("invalid_signature"), {error, "invalid_signature"};
+    %     {ok, claimsJSON} -> io:format("claimsJSON"), {ok, "claimsJSON"}
     end.
 
 description() -> "Auth jwt Module".
